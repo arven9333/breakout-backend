b776ea0224e09ed21e3a70d2b76421400f090c95
Nikita Zavertaylo <nikita.zavertaylo@ucell.uz>
2024-09-24 04:48:09 +0300

    asd

--- src/app/utils/image_to_tiles.py
+++ src/app/utils/image_to_tiles.py
@@ -60,5 +60,34 @@ def gdal_to_tiles(file_path: str, save_dir: Path):
     gdal2tiles.generate_tiles(input_file=file_path, output_folder=str(save_dir), **options)
 
 
+def image_to_tiles(image_path: str, tile_dir: Path):
+    with Image.open(image_path) as orig_world_map:
+        for z in range(MIN_ZOOM, MAX_ZOOM + 1):
+            if z == 0:
+                print("Zoom level", z, "- Generating", 2 ** z * 2 ** z, "256x256 image")
+            else:
+                print("Zoom level", z, "- Generating", 2 ** z * 2 ** z, "256x256 images")
+
+            tiles_per_dimension = 2 ** z
+            # Zoom the world map to be big enough for the number of tiles given the size of the tiles
+            zoomed_world_map = orig_world_map.resize((
+                tiles_per_dimension * TILE_SIZE,
+                tiles_per_dimension * TILE_SIZE,
+            ))
+            for x in range(tiles_per_dimension):
+                x_val_dir = os.path.join(tile_dir, str(z), str(x))
+                if not os.path.exists(x_val_dir):
+                    os.makedirs(x_val_dir)
+                for y in range(tiles_per_dimension):
+                    cropped = zoomed_world_map.crop((
+                        x * TILE_SIZE,
+                        y * TILE_SIZE,
+                        (x + 1) * TILE_SIZE - 1,
+                        (y + 1) * TILE_SIZE - 1,
+                    ))
+                    cropped.save(os.path.join(x_val_dir, str(y) + ".png"))
+
+
 #image_to_tiles('/home/ven9/edd0cdf5668043a583ebb1b239622fc0.png', './tiles')
-#gdal_to_tiles('/home/ven9/Рабочий стол/download.png', './tiles')
+#image_to_tiles('/home/ven9/edd0cdf5668043a583ebb1b239622fc0.png', './tiles')
+#gdal_to_tiles('/home/ven9/projects/Ilkhom/tarkov_fixed/breakout-backend/src/images/maps/4/102/tiles/ecb216f6cb8a482483da3a6bea112dcf.png', './tiles')
