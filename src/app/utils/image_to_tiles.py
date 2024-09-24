a97c19a6ab8ec0549a5ee628b92fa498c8351994
Nikita Zavertaylo <nikita.zavertaylo@ucell.uz>
2024-09-24 06:52:14 +0300

    asd

--- src/app/utils/image_to_tiles.py
+++ src/app/utils/image_to_tiles.py
@@ -28,6 +28,7 @@ def generate_tiles(stream: bytes, path: str, format_str: str):
 
     bytes_img = io.BytesIO(stream)
 
+    print(f"Начал сохранение {temp_file_path}")
     if format_str == 'svg':
         with WandImage(blob=bytes_img.read()) as image:
             image.format = 'png'
@@ -36,16 +37,16 @@ def generate_tiles(stream: bytes, path: str, format_str: str):
     else:
         with Image.open(bytes_img) as orig_world_map:
             orig_world_map.save(temp_file_path)
-
+    print(f"Сохранил {temp_file_path}")
     try:
         gdal_to_tiles(file_path=temp_file_path, save_dir=path)
     except Exception as e:
         print(e)
     finally:
-        #os.remove(temp_file_path)
-        print(temp_file_path)
+        os.remove(temp_file_path)
         if os.path.exists(path / 'tilemapresource.xml'):
             os.remove(path / 'tilemapresource.xml')
+        print(f"Удалил {temp_file_path}")
 
 
 def gdal_to_tiles(file_path: str, save_dir: Path):
