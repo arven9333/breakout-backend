96d30b6aa605abefefb4236272d3cb102662f250
Nikita Zavertaylo <nikita.zavertaylo@ucell.uz>
2024-09-30 17:04:04 +0300

    asd

--- src/app/main.py
+++ src/app/main.py
@@ -79,6 +79,7 @@ async def lifespan(app: FastAPI):
 
     logger.addHandler(QueueHandler(que))
 
+    await run_app(app, settings.api_config)
     listener = QueueListener(que, logging.StreamHandler())
     listener.start()
     logger.debug(f'Logger has started')
@@ -94,5 +95,5 @@ setup_logging(__name__)
 
 app = create_app(settings)
 
-if __name__ == "__main__":
-    asyncio.run(run_app(app, settings.api_config))
+# if __name__ == "__main__":
+#     asyncio.run(run_app(app, settings.api_config))
